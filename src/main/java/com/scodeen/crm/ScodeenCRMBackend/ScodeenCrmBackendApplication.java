96ad1d9f2ae44f927bf3d61b1f2c1960a9c12141
Kedar Bhanegaonkar <ksbhanegaonkar@gmail.com>
2019-12-21 19:13:39 +0530

    Initial commit..

---
+++ src/main/java/com/scodeen/crm/ScodeenCRMBackend/ScodeenCrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.scodeen.crm.ScodeenCRMBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScodeenCrmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScodeenCrmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scodeen/crm/ScodeenCRMBackend/ScodeenCrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scodeen.crm.ScodeenCRMBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScodeenCrmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
