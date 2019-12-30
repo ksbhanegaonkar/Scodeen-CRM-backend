00bad7bdd781b5ec1b9f2b810a3ed3554bc99909
Kedar Bhanegaonkar <ksbhanegaonkar@gmail.com>
2019-12-30 22:52:28 +0530

    Added SqlServer integration. Working fine

--- src/main/java/com/scodeen/controller/RegistrationController.java
+++ src/main/java/com/scodeen/controller/RegistrationController.java
@@ -1,17 +1,14 @@
 package com.scodeen.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @RestController
 public class RegistrationController {
 	
-    @PostMapping("/registercandidate")
-    public String registerCandidate(@RequestBody String action) {
+    @GetMapping("/registercandidate")
+    public String registerCandidate() {
 
         return "Success";    
         }
---
+++ src/main/java/com/scodeen/repository/a.java
@@ -0,0 +1,5 @@
+package com.scodeen.repository;
+
+public class a {
+
+}
